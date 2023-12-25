ba89c6d056c6aea4b4069f4222fb622c48b5fa2a
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-12-25 22:29:44 +0300

    Create PartA-Types1.ts

---
+++ PartA-Types1.ts
@@ -0,0 +1,24 @@
+//string type 
+
+let helloTypeScript: string = "Hi there"
+
+helloTypeScript = 'Hi again' 
+
+console.log(helloTypeScript) //Hi again
+
+//number type
+
+let year: number = 2023
+
+let yourETH: number = 19.99
+
+//boolean type
+
+let areYouReady: boolean = true
+
+let isCompleted: boolean = false
+
+//tsc yourFile.ts --w
+//node yourFile.js
+
+export {};
