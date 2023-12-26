80bb34b8b2c5d540d82d5ef1f1de9a3b5eadfa7a
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-12-26 15:14:18 +0300

    Create PartA-Types2.ts

---
+++ PartA-Types2.ts
@@ -0,0 +1,13 @@
+//any type
+
+//know any, but don't use it:)
+
+let userPassword: any
+
+userPassword = 345210
+
+console.log(typeof(userPassword)) //number
+
+userPassword = '345210'
+
+console.log(typeof(userPassword)) //string
