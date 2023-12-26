da6508b58b0ca7afff0b64f35f49aada2af848fe
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-12-26 15:37:54 +0300

    Create PartA-Types3.ts

---
+++ PartA-Types3.ts
@@ -0,0 +1,31 @@
+//number array
+
+let numbers: number[] = [0, 1, 2 , 4]
+
+//string array
+
+let smartContractLangs: string[] = ["Solidity", "TypeScript", "Move"]
+
+//we can log arrays
+
+console.log(smartContractLangs)
+
+//array of strings and numbers
+
+let personInfo: (string | number)[] = ["mustafa", 2024]
+
+//array of strings, numbers and booleans
+
+let tripleMixed: (string | number | boolean)[] = ["mustafa", 2024, true, false]
+
+//nested number array
+
+let nestedNums: number[][] = [[0, 1, 2], [2023, 2024], [-500, -10100]]
+
+//nested string array
+
+let nestedStrs: string[][] = [["mustafa", "buyukdereli"], ["0", '1', '2']]
+
+//nested string and number type array
+
+let mixNestedArr: (string | number)[][] = [[1, 2, 3], ["M", 'b']]
