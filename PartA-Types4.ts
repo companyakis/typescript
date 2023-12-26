ea515e5caccb8dee39d4375192eed7555ce6364f
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-12-26 15:59:23 +0300

    Create PartA-Types4.ts

---
+++ PartA-Types4.ts
@@ -0,0 +1,9 @@
+//tuple
+
+let giveInfo: [string, number, boolean] = ["mustafa", 2023, true]
+
+//let giveInfo2: [string, number, boolean] = ["mustafa", 2023, true, "hi there"] // Source has 4 element(s) but target allows only 3
+
+//let's use const
+
+const myInfo = ["mustafa", 2023, true]
