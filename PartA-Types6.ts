d1c8e711f3d0658cf8481d9cb3f73fd906995cfe
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-12-26 16:22:18 +0300

    Create PartA-Types6.ts

---
+++ PartA-Types6.ts
@@ -0,0 +1,12 @@
+//string enums
+
+const enum HerInfo {
+    Name = "Aygun",
+    University = "MIT"
+}
+
+console.log(HerInfo.Name)
+
+console.log(HerInfo.University)
+
+let herName: HerInfo = HerInfo.Name
