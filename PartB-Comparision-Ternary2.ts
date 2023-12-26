baf07b0eb1c756e9a8be7210b6f499e956f47128
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-12-26 20:27:07 +0300

    Create PartB-Comparision-Ternary2.ts

---
+++ PartB-Comparision-Ternary2.ts
@@ -0,0 +1,8 @@
+let examResult = 86
+
+let teacherSays = (examResult < 60) ? "Bad!" :
+                  (examResult < 70) ? "Study please" :
+                  (examResult < 80) ? "Study hard" :
+                  "Thank you"
+
+console.log(teacherSays)
