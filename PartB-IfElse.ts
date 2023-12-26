deb53a3a14646961b3781a8b1c44853d4ac0f2e5
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-12-26 20:02:41 +0300

    Create PartB-IfElse.ts

---
+++ PartB-IfElse.ts
@@ -0,0 +1,11 @@
+let a: number = -122
+
+let b: number = 122
+
+if (a > b) {
+    console.log("a is bigger")
+} else if (b > a) {
+    console.log("b is bigger")
+} else {
+    console.log("They are equal")
+}
