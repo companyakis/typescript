ee3428064733e1bf87792403d159114c50607f79
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-12-28 10:43:42 +0300

    Create PartE-BasicTypes2-UndefinedNull.ts

---
+++ PartE-BasicTypes2-UndefinedNull.ts
@@ -0,0 +1,15 @@
+let unknownVariable: number
+
+//console.log(unknownVariable) //undefined
+
+let unknownVariable2
+
+//console.log(unknownVariable2) //undefined
+
+let unknownVariable3 = null
+
+console.log(unknownVariable3) //null
+
+unknownVariable3 = 23
+
+console.log(unknownVariable3) //23
